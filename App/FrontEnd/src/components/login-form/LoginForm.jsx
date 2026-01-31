4cd649f6020b42c9b4e7bb365f16fcfed8c35c9c
johnnyboyy21 <jchiem007@hotmail.com>
2026-01-30 19:20:41 -0500

    login form, button and submission logic missing

---
+++ App/FrontEnd/src/components/login-form/LoginForm.jsx
@@ -0,0 +1,61 @@
+import "./LoginForm.css"
+
+function LoginForm(){
+    const [firstname, setFirstName] = useState("");
+    const [lastname, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    
+    function handleFirstName(e){
+        setFirstName(e.target.value);
+    }
+
+    function handleLastName(e){
+        setLastName(e.target.value);
+    }
+
+    function handleEmail(e){
+        setEmail(e.target.value);
+    }
+
+    function handlePassword(e){
+        setPassword(e.target.value);
+    }
+
+    return (
+        <div>
+            <h1> Login Form </h1>
+            <label> First Name: 
+                <input
+                    type="text"
+                    value={firstname}
+                    onChange={handleFirstName}
+                    />
+            </label>
+
+            <label> Last Name: 
+                <input
+                type="text"
+                value={lastname}
+                onChange={handleLastName}
+                />
+            </label>
+
+            <label> Email: 
+                <input
+                type="text"
+                value={email}
+                onChange={handleEmail}
+                />
+            </label>
+            
+            <label> Password: 
+                <input
+                type="text"
+                value={password}
+                onChange={handlePassword}
+                />
+            </label>
+        </div>
+    )
+}
