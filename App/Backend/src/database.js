b8d7286bf11d9a9b00f511ada2dd542dea1e0835
matthewvcadogan <matthew.vcadogan@gmail.com>
2026-02-06 18:34:14 -0500

    boilerplate for routers and database

---
+++ App/Backend/src/database.js
@@ -0,0 +1,2 @@
+// This file has the responsobility of communicating (reading/writing) with the database (in this case, a JSON file)
+// This file will create and export the necessary functions to read and write to the JSON file, which will be used by other files.
