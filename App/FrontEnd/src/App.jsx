bfad00e23dfe718092bb2705fb2f2574dec52117
mark-samuel2 <marksamuel228@gmail.com>
2026-02-06 18:23:17 -0500

    recipe manager component created

--- App/FrontEnd/src/App.jsx
+++ App/FrontEnd/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import SignUpForm from './components/signup-form/SignUpForm.jsx';
+import recipeManager from './components/recipe-manager/RecipeManager.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-    <SignUpForm></SignUpForm>   // recipe manager
+    <recipeManager></recipeManager>
     </div>
   )
 }
