ba8c506dd8d39ff87f196fd84ca9a905b0dbae65
matthewvcadogan <matthew.vcadogan@gmail.com>
2026-02-13 19:08:29 -0500

    fixed error caused by leftover code for data.json (now users.json)

--- App/Backend/src/app.js
+++ App/Backend/src/app.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const fs = require('fs');
-const path = require('path');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-
-// Load data from data.json
-const dataPath = path.join(__dirname, '../../Data/data.json');
-let userData = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
 
 const PORT = process.env.PORT || 3000;
 
