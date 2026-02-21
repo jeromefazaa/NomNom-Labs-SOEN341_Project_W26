2d7bab92788ad275065aacd3d86763ad525cec45
johnnyboyy21 <jchiem007@hotmail.com>
2026-02-20 19:44:30 -0500

    added button to logout or edit profile if logged

--- App/FrontEnd/src/components/recipe-manager/profile/user-management/UserManagement.jsx
+++ App/FrontEnd/src/components/recipe-manager/profile/user-management/UserManagement.jsx
@@ -4,6 +4,14 @@ import ManageProfileButton from './profile-management/ManageProfileButton.jsx'
 function UserManagement({ onLogout, isLoggedIn }) {
     return (
         <div>
+            <button>
+                Logout
+            </button>
+
+            <button>
+                My Profile
+            </button>
+            
             {isLoggedIn && <LogoutButton onLogout={onLogout}></LogoutButton>}
             <ManageProfileButton></ManageProfileButton>
         </div>
