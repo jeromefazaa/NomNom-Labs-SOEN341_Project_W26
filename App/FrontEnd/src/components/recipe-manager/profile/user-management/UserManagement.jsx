00e983e9156e0d1700397dd139fcbb4937dc25e4
johnnyboyy21 <jchiem007@hotmail.com>
2026-02-22 19:15:46 -0500

    trying logout from user management

--- App/FrontEnd/src/components/recipe-manager/profile/user-management/UserManagement.jsx
+++ App/FrontEnd/src/components/recipe-manager/profile/user-management/UserManagement.jsx
@@ -1,12 +1,27 @@
 import './UserManagement.css'
 import LogoutButton from './logout-button/LogoutButton.jsx'
 import ManageProfileButton from './profile-management/ManageProfileButton.jsx'
-import { logout } from "App/FrontEnd/"
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { logout } from "../../../../../redux/slices/appStateSlice";
+
 
 function UserManagement({ onLogout, isLoggedIn }) {
+    const dispatch = useDispatch();
+
+    function handleLogout(e) {
+        e?.preventDefault();
+
+        dispatch(logout());
+
+        if (onLogout) {
+            onLogout(); //closes popup
+        }
+    }
+
     return (
         <div>
-            <button>
+            <button type="button" onClick={handleLogout}>
                 Logout
             </button>
 
