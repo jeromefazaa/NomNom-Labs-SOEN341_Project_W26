569f390c31d41cbd0366d1b0244e7ee466b37f63
mark-samuel2 <marksamuel228@gmail.com>
2026-02-25 16:39:26 -0500

    recipe backend fix

--- App/Backend/src/routers/recipes/recipes.js
+++ App/Backend/src/routers/recipes/recipes.js
@@ -8,16 +8,19 @@ const router = express.Router();
 router.post("/:userId", (req, res) => {
     console.log(`receied request`)
     let recipes = db.read('recipes');
+    console.log(`recipes in db:${JSON.stringify(recipes)}`)
     const userId = req.params.userId; 
-    const recipeData = req.body.recipeData
-    const newUserRecipes = req.body.recipes;
-    const userRecipes = Object.keys(recipes).filter(id => id.startsWith(userId));
-    userRecipes.forEach(recipe => delete recipes[recipe])
+    const newUserRecipes = req.body.recipeData
+    const userRecipes = Object.keys(recipes).filter(id => {
+        const email = id.split("_")[0];
+        return email === userId
+    });
+    userRecipes.forEach(recipe => delete recipes[recipe]);
     recipes = {
         ...recipes,
         ...newUserRecipes
     }
-    if (db.write('recipes', recipeData)) {
+    if (db.write('recipes', recipes)) {
         res.status(200).json({ message: 'Recipes saved successfully' });
     } else {
         res.status(500).json({ error: 'Failed to save recipe' });
