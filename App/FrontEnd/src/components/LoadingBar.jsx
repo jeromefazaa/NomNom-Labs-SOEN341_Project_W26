b6a21e28045990474e4c1fbc2220a08820c62881
mark-samuel2 <marksamuel228@gmail.com>
2026-02-27 18:01:16 -0500

    implemented continuous integration

--- App/FrontEnd/src/components/LoadingBar.jsx
+++ App/FrontEnd/src/components/LoadingBar.jsx
@@ -2,19 +2,19 @@ import { useSelector } from "react-redux";
 import "../styles/LoadingBar.css";
 
 function LoadingBar() {
-  const appIsLoading = useSelector((state) => state.appState?.isLoading );
-  const recipesIsLoading = useSelector( (state) => state.recipes?.isLoading);
+  const appIsLoading = useSelector((state) => state.appState?.isLoading);
+  const recipesIsLoading = useSelector((state) => state.recipes?.isLoading);
 
   const userIsLoading = useSelector((state) => state.currentUser?.isLoading);
 
   const showLoadingBar = appIsLoading || recipesIsLoading || userIsLoading;
-  debugger;
+  //debugger;
   return (
     <>
       {showLoadingBar && (
         <div className="loading-bar-container">
           <div className="loading-bar"></div>
-          <h1  style={{color: 'black'}} >Hi I am a loading bar</h1>
+          <h1 style={{ color: "black" }}>Hi I am a loading bar</h1>
         </div>
       )}
     </>
