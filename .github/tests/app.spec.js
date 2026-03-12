acce29aef43fc8f5d72f7ecadd930908804dbfcc
mark-samuel2 <marksamuel228@gmail.com>
2026-03-12 18:06:54 -0400

    trying CI

--- .github/tests/app.spec.js
+++ .github/tests/app.spec.js
@@ -1,12 +1,13 @@
 const { test, expect } = require("@playwright/test");
 test("MealMajor Journey: Register and Redirect", async ({ page }) => {
   // 1. Visit the registration page
-  await page.goto("http://localhost:3000/register.html");
+  await page.goto("http://localhost:3000/login");
   // 2. Fill out the form
-  const uniqueEmail = `user_${Date.now()}@concordia.ca`;
-  await page.fill("#email", uniqueEmail);
-  await page.fill("#password", "securePass123");
+  //const uniqueEmail = `user_${Date.now()}@concordia.ca`;
+  await page.fill("#email", "marksam@gmail.com");
+  await page.fill("#password", "Mark123456");
   // 3. Submit and verify redirect to login
   await page.click("#registerBtn");
-  await expect(page).toHaveURL(/.*login.html/);
+  await expect(page).toHaveURL(/.*login/);
 });
+// login instead of signup
