edefd4986a40984fec2309c5b0cf5541a3984a99
mark-samuel2 <marksamuel228@gmail.com>
2026-03-13 13:21:22 -0400

    testing CI

--- .github/tests/app.spec.js
+++ .github/tests/app.spec.js
@@ -4,10 +4,11 @@ test("MealMajor Journey: Register and Redirect", async ({ page }) => {
   await page.goto("http://localhost:3000/auth/login");
   // 2. Fill out the form
   //const uniqueEmail = `user_${Date.now()}@concordia.ca`;
-  await page.fill("email", "marksam@gmail.com");
-  await page.fill("password", "Mark123456");
+ await page.fill('input[type="email"]', "marksam@gmail.com");
+  await page.fill('input[type="password"]', "Mark123456");
   // 3. Submit and verify redirect to login
-  await page.click("btn btn-primary");
+  await page.click(".btn.btn-primary");
+
   await expect(page).toHaveURL(/.*login/);
 });
 // login instead of signup
