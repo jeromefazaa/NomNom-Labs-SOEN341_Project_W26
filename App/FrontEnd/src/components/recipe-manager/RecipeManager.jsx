2caa5447fc4409c19066ec4da942d940143505d0
mark-samuel2 <marksamuel228@gmail.com>
2026-03-13 14:14:24 -0400

    trying CI

--- App/FrontEnd/src/components/recipe-manager/RecipeManager.jsx
+++ App/FrontEnd/src/components/recipe-manager/RecipeManager.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-import Profile from './profile/Profile.jsx'
-import MainPage from './main-page/MainPage.jsx'
-import SaveButton from './save/SaveButton.jsx'
-import { IconButton } from '@mui/material';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { useState } from "react";
+import Profile from "./profile/Profile.jsx";
+import MainPage from "./main-page/MainPage.jsx";
+import SaveButton from "./save/SaveButton.jsx";
+import { IconButton } from "@mui/material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle.js";
 
 function RecipeManager() {
   const [profileOpen, setProfileOpen] = useState(false);
@@ -18,14 +18,18 @@ function RecipeManager() {
 
   return (
     <div>
-      <IconButton onClick={handleProfileClick} color="primary" className="profile-icon">
-      <AccountCircleIcon />
+      <IconButton
+        onClick={handleProfileClick}
+        color="primary"
+        className="profile-icon"
+      >
+        <AccountCircleIcon />
       </IconButton>
       <MainPage></MainPage>
       <SaveButton></SaveButton>
       <Profile open={profileOpen} onClose={handleProfileClose} />
     </div>
-  )
+  );
 }
 
-export default RecipeManager
+export default RecipeManager;
